54542695661ff18030a2cc1f478d4e672966b072
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2022-06-30 17:14:05 +0300

    add files

---
+++ Sources/MyLibrary/OnBoardingAssets.swift
@@ -0,0 +1,60 @@
+//
+//  OnBoardingAssets.swift
+//  OnBoardingKit
+//
+//  Created by Ahror Jabborov on 6/30/22.
+//
+
+import Foundation
+import SwiftUI
+
+public struct ViewAssets {
+    var titleFont: String
+    var descriptionFont: String
+    var mainTextColor: UIColor
+    var descriptionTextColor: UIColor
+    var detailsTextColor: UIColor
+    var copyrightTextColor: UIColor
+    var placeholderTextColor: UIColor
+    var primaryColor: UIColor
+    var primaryLightColor: UIColor
+    var primaryLighterColor: UIColor
+    var buttonTextColor: UIColor
+    var borderColor: UIColor
+    var completeProgressColor: UIColor
+    var pinkAccentColor: UIColor
+    var successMain: Color
+    var successLighter: Color
+    var errorMain: Color
+    var errorLighter: Color
+    /// icons
+    var timeImage: String
+    var filterButtonImage: String
+    var authorIcon: String
+    var durationOnIcon: String
+    var durationOffIcon: String
+    var beginCourseButtonIcon: String
+    var videoIcon: String
+    var documentIcon: String
+    var quizIcon: String
+    var courseDetailBackground: String
+    var onBoardingButtonIcon: String
+    var bookLogo: String
+    var notificationIcon: String
+    var email: String
+    var password: String
+    var user: String
+    var checkboxFull: String
+    var checkboxEmpty: String
+    var eyeCrossed: String
+    var completeCourseIcon: String
+    var placeholder: String
+    var cross: String
+    var asteriks: String
+    var asteriksCircle: String
+    var questionMark: String
+    var quizHeader: String
+    var notificationBell: String
+    var quizPopup: String
+    var quizPopupImage: String
+}
