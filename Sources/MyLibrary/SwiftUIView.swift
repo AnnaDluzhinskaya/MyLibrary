1198deb06aa4a10ce820c038e82dc50e799b57c0
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2022-06-30 16:29:57 +0300

    add some data

---
+++ Sources/MyLibrary/SwiftUIView.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Anna Dluzhinskaya on 30.06.2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .frame(width: UIScreen.main.bounds.width-60, height: UIScreen.main.bounds.height/2, alignment: .center)
+                .foregroundColor(.blue)
+            Text("Hi, I am Ann!")
+        }.cornerRadius(20)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
